77a249e7907798017ece9f74c5d8ba4b01ff2d13
sergioariasblanco <sergio.arias@alumnos.upm.es>
2021-05-08 17:02:30 +0200

    ADICCIÓN DE CÓDIGO EL PRINCIPAL
    Se ha añadido al código principal el código para escoger el número de jugadores de  la partida y también el código que almacena los nombres de cada jugador.

--- MENÚPÓKER.c
+++ MENÚPÓKER.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 void opcion_a(void);
 void opcion_b(void);
 int main()
@@ -32,9 +34,54 @@ int main()
       fclose(fp);
        		break;
  	case 2:
-	    printf("Preparese para configurar su juego...");
-	    //Aqui iria el resto del programa
-	    break;
+	    {printf("Preparese para configurar su juego...\n\n");
+        printf("Escoja un número de jugadores entre 2 y 6 jugadores.\n\n");
+          int n;
+          scanf("%i", &n);
+          switch(n)
+           {
+            case 2:
+                printf("El número de jugadores es 2.\n\n");
+                    char jugador1[50], jugador2[50];
+                    {printf("Escribe el nombre de cada jugador.\n\n");
+                    scanf("%s %s", jugador1, jugador2);
+                    printf("Los nombres de los jugadores son: jugador1: %s y jugador2: %s.\n\n", jugador1, jugador2);
+                    break;
+                    }
+            case 3:
+                printf("El número de jugadores es 3.\n\n");
+                    char jugador11[50], jugador22[50], jugador3[50];
+                    {printf("Escribe el nombre de cada jugador.\n\n");
+                    scanf("%s %s %s", jugador11, jugador22, jugador3);
+                    printf("Los nombres de los jugadores son: jugador1: %s, jugador2: %s y jugador3: %s.\n\n", jugador11, jugador22, jugador3);
+                    break;
+                    }
+            case 4:
+                printf("El número de jugadores es 4.\n\n");
+                    char jugador111[50], jugador222[50], jugador33[50], jugador4[50];
+                    {printf("Escribe el nombre de cada jugador.\n\n");
+                    scanf("%s %s %s %s", jugador111, jugador222, jugador33, jugador4);
+                    printf("Los nombres de los jugadores son: jugador1: %s, jugador2: %s, jugador3: %s, jugador4: %s.\n\n", jugador111, jugador222, jugador33, jugador4);
+                    break;
+                    }
+            case 5:
+                printf("El número de jugadores es 5.\n\n");
+                    char jugador1111[50], jugador2222[50], jugador333[50], jugador44[50], jugador5[50];
+                    {printf("Escribe el nombre de cada jugador.\n\n");
+                    scanf("%s %s %s %s %s", jugador1111, jugador2222, jugador333, jugador44, jugador5);
+                    printf("Los nombres de los jugadores son: jugador1: %s, jugador2: %s, jugador3: %s, jugador4: %s, jugador5: %s.\n\n", jugador1111, jugador2222, jugador333, jugador44, jugador5);
+                    break;
+                    }
+            case 6:
+                printf("El número de jugadores es 6.\n\n");
+                    char jugador11111[50], jugador22222[50], jugador3333[50], jugador444[50], jugador55[50], jugador6[50];
+                    {printf("Escribe el nombre de cada jugador.\n\n");
+                    scanf("%s %s %s %s %s %s", jugador11111, jugador22222, jugador3333, jugador444, jugador55, jugador6);
+                    printf("Los nombres de los jugadores son: jugador1: %s, jugador2: %s, jugador3: %s, jugador4: %s, jugador5: %s, jugador6: %s.\n\n", jugador11111, jugador22222, jugador3333, jugador444, jugador55, jugador6);
+                    break;
+                    }
+           }
+       } break;
 	case 3:
 	    printf("Vuelva pronto \n");
 		exit(0);

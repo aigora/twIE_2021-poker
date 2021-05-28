1679c238c77cb150cbdd3b24f45b21e77747072a
aamontoya <a.amontoya@alumnos.upm.es>
2021-05-28 19:12:50 +0200

    Fusión código apuestas y cartas sobre la "mesa"
    Fusión del código final del repartidor con el código de las rondas de apuestas

--- Código+mano_de_cartas.c
+++ Código+mano_de_cartas.c
@@ -21,8 +21,17 @@ void temp(int n);
 void ssaldo ();
 int main()
 {
- int a,b,c,X,z,max,njugadores,longiN,i;
- int bote_ganador=0;
+ int a,b,c,X,njugadores,longiN,i;
+ int bote_ganador;
+ srand(time(0));
+ char palo[4][10] = {"Treboles", "Diamantes", "Corazones", "Espadas"};
+ char cara[14] = {'1','2','3','4','5','6','7','8','9','10','J', 'Q', 'K', 'A'};
+ int num, l;
+ int elecc[5][2];
+ int j, paloI, caraI, dup;
+ int selected = 0;
+ bool deck[4][13] = {false};
+
  usuario jugador[MAXJUG];
  FILE *fp;
  char ch;
@@ -57,23 +66,15 @@ int main()
             {
                 do
                 {
-                    printf("Escribe el nombre del jugador %d (30 caracteres como maximo):",i+1);
+                    printf("Escribe el nombre del jugador %d :",i+1);
                     scanf("%s",jugador[i].nombre);
 		            longiN=strlen(jugador[i].nombre);
 		        }while(longiN>30);
 	        }
-        printf("Prepárense para recibir sus cartas...cada jugador dispone de 5 segundos para visualizarlas\n\n");
+            printf("Prepárense para recibir sus cartas...cada jugador dispone de 5 segundos para visualizarlas\n\n");
 
                     temp(7);
-                    srand(time(0));
-
-                    char palo[4][10] = {"Treboles", "Diamantes", "Corazones", "Espadas"};
-                    char cara[3] = {'J', 'Q', 'K'};
-
-                    bool deck[4][13] = {false};
-
                     system("cls");//borra todo lo anterior impreso en pantalla
-
                     for ( i = 0; i < njugadores; i++)
                     {
                         printf("Mano de %s es:\n",jugador[i].nombre);
@@ -113,6 +114,7 @@ int main()
                     printf("Es el turno de %s, como eres el primer jugador debes pagar la ciega pequeña.\n",jugador[0].nombre);
                     system("pause");
                     printf("%s ha pagado la ciega pequeña ($5).\n",jugador[0].nombre);
+                    bote_ganador=0;
                     jugador[0].saldo = jugador[0].saldo - R;
                     bote_ganador = bote_ganador + R;
                     printf("El saldo actual de %s es de %i.\n\n", jugador[0].nombre, jugador[0].saldo);
@@ -124,7 +126,6 @@ int main()
                     bote_ganador = bote_ganador + A;
                     printf("El saldo actual de %s es de %i.\n\n", jugador[1].nombre, jugador[1].saldo);
                     jugador[0].llevapostado=5;
-                    max=jugador[0].llevapostado;
                     jugador[1].llevapostado=10;
                     X=A;
                     if (njugadores>=3 && njugadores<=6)
@@ -198,8 +199,35 @@ int main()
                                 }
                             }
                         }
-                        //CODIGO PABLO 1RA RONDA (3 CARTAS)
-                    printf("\n");
+                    printf("\n Se da por finalizada la primera ronda de apuestas\n");
+                    printf("Prepárense para ver las tres primeras cartas...");
+                    printf("\n\n");
+                    temp(3);
+                    getchar();
+	                for(i = 0; i < 3; i++)
+                        {
+		                   do{
+		                      paloI = rand() % 4;
+		                      caraI = rand() % 3;
+                              dup = 0;
+		                   for(j = 0; j < i; j++)
+		                   {
+		                       if(paloI == elecc[j][0] && caraI == elecc[j][1])
+		                       dup = 1;
+				               break;
+		                   }
+                           }
+                           while(dup != 0);
+                           elecc[i][0] = paloI;
+                           elecc[i][1] = caraI;
+                        }
+                        printf("\n");
+                        for(i = 0; i < 3; i++)
+                            {
+                                printf("%s-%c\t", palo[elecc[i][0]], cara[elecc[i][1]]);
+                            }
+
+                    printf("\n\n");
                     printf("Esta es la segunda ronda de apuestas o Flop.\n\n");
                     for(i=0;i<njugadores;i++)
                             {
@@ -251,8 +279,35 @@ int main()
                                         break;
                                 }
                                 }
-                        //CODIGO PABLO 2NDA RONDA (4 CARTAS)
-                    printf("\n");
+                        printf("\n Se da por finalizada la segunda ronda de apuestas\n");
+                        printf("Prepárense para ver la cuarta carta...");
+                        temp(3);
+                        printf("\n\n");
+                        getchar();
+                        do
+                            {
+                                paloI = rand() % 4;
+                                caraI = rand() % 3;
+                                dup = 0;
+                                for(j = 0; j < 4; j++)
+                                    {
+                                        if(paloI == elecc[j][0] && caraI == elecc[j][1])
+                                        {
+                                            dup = 1;
+                                            break;
+                                        }
+                                    }
+                            }
+                        while(dup != 0);
+                        elecc[3][0] = paloI;
+                        elecc[3][1] = caraI;
+	                    printf("\n");
+	                    for(i = 0; i < 4; i++)
+	                    {
+	                        printf("%s-%c\t", palo[elecc[i][0]], cara[elecc[i][1]]);
+                        }
+
+                    printf("\n\n");
                     printf("Esta es la tercera ronda de apuestas o Turn.\n\n");
                     for(i=0;i<njugadores;i++)
                             {
@@ -304,8 +359,36 @@ int main()
                                         break;
                                 }
                                 }
-                        //CODIGO PABLO 3RA RONDA (5 CARTAS)
-                    printf("\n");
+                        printf("\n Se da por finalizada la tercera ronda de apuestas\n");
+                        printf("Prepárense para ver la quinta carta...");
+                        temp(3);
+                        printf("\n\n");
+                        getchar();
+                        do
+                            {
+                                paloI = rand() % 4;
+                                caraI = rand() % 3;
+                                dup = 0;
+                                for(j = 0; j < 3; j++)
+                                    {
+                                        if(paloI == elecc[j][0] && caraI == elecc[j][1])
+                                        {
+                                            dup = 1;
+                                            break;
+                                        }
+                                    }
+                            }
+                            while(dup != 0);
+                            elecc[4][0] = paloI;
+                            elecc[4][1] = caraI;
+                            printf("\n");
+                            for(i = 0; i < 5; i++)
+                                {
+                                    printf("%s-%c\t", palo[elecc[i][0]], cara[elecc[i][1]]);
+                                }
+
+
+                    printf("\n\n");
                     printf("Esta es la ronda final de apuestas o Showdown.\n\n");
                     for(i=0;i<njugadores;i++)
                             {
@@ -357,7 +440,7 @@ int main()
                                         break;
                                 }
                                 }
-                    //IMPRIMIR LAS CARTAS DE TODOS LOS JUGADORES Y CODIGO DENIZ DICIENDO QUIEN GANO + CODIGO ALE DICIENDO CUANRO GANO
+                    //IMPRIMIR LAS CARTAS DE TODOS LOS JUGADORES Y CODIGO DENIZ DICIENDO QUIEN GANO + CODIGO ALEXIA DICIENDO CUANTO GANO
 
 	    break;
 	case 3:
